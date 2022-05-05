cc3e3b2e99254063d3afb6e665501759c0713845
DelFaa365 <Tomasinka123@gmail.com>
2022-05-05 11:41:55 +0200

    Stepanova uprava jakoze

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sken = new Scanner(System.in);
+        System.out.print("Zadej počet lamp: ");;
+        int [] lampy = new int[sken.nextInt()];
+        System.out.print("Zadej vzdálenosti jednotlivých lamp od začátku ulice: ");
+        for (int i = 0; i < lampy.length; i++) {
+            lampy[i] = sken.nextInt();
+        }
+        Arrays.sort(lampy);
+        int mezera = 0;
+        for (int i = 0; i < lampy.length-1; i++) {
+            if(lampy[i+1]-lampy[i] > mezera){
+                mezera = lampy[i+1]-lampy[i];
+            }
+        }
+
+        double vysledek =(double)mezera/2;
+
+        System.out.println(vysledek);
     }
 }
